99179b024c85c05804b5c4a7a789962a568c8268
CarlosCante <carloscante@gmail.com>
2017-12-23 22:14:19 -0600

    Tarea 3

---
+++ Tarea 3/Tarea3/Tarea3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tarea3
+{
+    public partial class Form1 : Form
+    {
+        Arbol arbol = new Arbol();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arbol.Insertar(arbol.Raiz, new Nodo(textBox1.Text.ElementAt(0)));
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = arbol.PreOrden(arbol.Raiz);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = arbol.EnOrden(arbol.Raiz);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = arbol.PostOrden(arbol.Raiz);
+        }
+
+    }
+}
---
+++ Tarea 3/Tarea3/Tarea3/Nodo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarea3
+{
+    class Nodo
+    {
+        public char Caracter;
+        public Nodo der;
+        public Nodo izq;
+
+        public Nodo(char c)
+        {
+            Caracter = c;
+            der = null;
+            izq = null;
+        }
+    }
+
+    class Arbol
+    {
+        public Nodo Raiz;
+
+        public Arbol()
+        {
+            Raiz = null;
+        }
+
+        public void Insertar(Nodo r, Nodo nuevo)
+        {
+
+            if (Raiz == null) // Si se llego a un nodo nulo, el nuevo nodo se inserta en ese espacio
+            {
+                Raiz = nuevo;
+                return;
+            }
+
+            if (nuevo.Caracter.CompareTo(r.Caracter) < 0) //Si el nuevo nodo es menor que el actual se decide seguir buscando por la derecha si lugar de insercion
+            {
+                if (r.izq == null)
+                    r.izq = nuevo;
+                else
+                    Insertar(r.izq, nuevo);
+            }
+            else if (nuevo.Caracter.CompareTo(r.Caracter) > 0)//Si el nuevo nodo es menor que el actual se decide seguir buscando por la izquierda si lugar de insercion
+            {
+                if (r.der == null)
+                    r.der = nuevo;
+                else
+                    Insertar(r.der, nuevo);
+            }
+        }
+
+        public string PreOrden(Nodo raiz)
+        {
+            string res = "";
+            if (raiz != null)
+            {
+                res += raiz.Caracter;
+                res += PreOrden(raiz.izq);
+                res += PreOrden(raiz.der);
+            }
+            return res;
+        }
+
+        public string EnOrden(Nodo raiz)
+        {
+            string res = "";
+            if (raiz != null)
+            {
+                res += EnOrden(raiz.izq);
+                res += raiz.Caracter;
+                res += EnOrden(raiz.der);
+            }
+            return res;
+        }
+
+        public string PostOrden(Nodo raiz)
+        {
+            string res = "";
+            if (raiz != null)
+            {
+                res += PostOrden(raiz.izq);
+                res += PostOrden(raiz.der);
+                res += raiz.Caracter;
+            }
+            return res;
+        }
+    }
+}
